14859fac0ace361827a8a9458230a915e1df26b3
arpchauh <arpitchauhan10@gmail.com>
2016-09-29 11:33:41 +0530

    Add files via upload

---
+++ Session3_Assignment1.java
@@ -0,0 +1,15 @@
+package tanaji;
+
+public class Session3_Assignment1 {
+
+	
+			 static{
+			 print(10);
+			 }
+			 static void print (int x) {
+			 System.out.println (x);
+			 System.exit(0);
+			 }
+			 }
+		
+//Compiler error since the main method is not used
